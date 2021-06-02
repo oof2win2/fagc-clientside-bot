f9e1363c027f6c336f69d05fe7ba8fca9c1a8e36
oof2win2 <honza.koco44@gmail.com>
2021-06-02 12:04:56 +0200

    :wastebasket: Removed useless files

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const { getStringFromID, getRegexFromString } = require("./utils/functions")
-
-const idStr = "60a8012a027b1c2365f298ad"
-const string = getStringFromID(idStr)
-
-console.log(string)
-const regex = getRegexFromString(string)
-console.log(regex)
