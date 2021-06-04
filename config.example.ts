2a577b2e98baa567400dd47c7fee1b6be29d1635
oof2win2 <honza.koco44@gmail.com>
2021-06-04 23:45:50 +0200

    :sparkles: Added example configs and lua side plugin

---
+++ config.example.ts
@@ -0,0 +1,33 @@
+import { BotConfig } from "./src/types/FAGCBot"
+
+const config: BotConfig = {
+	token: "", // Discord bot token
+	prefix: "banbot!", // Bot prefix
+	apiurl: "http://localhost:3000/v1", // Base API url
+	websocketurl: "ws://localhost:8001", // FAGC WS URL
+	adminIDs: [ // Admins user IDs that can restart the bot etc. Shouldn't be a large number of people
+		"429696038266208258"
+	],
+	embeds: { // Embed configs
+		color: "GREEN",
+		footer: "FAGC Team | oof2win2"
+	},
+	emotes: { // Some emotes
+		error: ":x:",
+		success: "<:success:841385407790317588>"
+	},
+	owner: {
+		id: "429696038266208258",
+		name: "oof2win2#3149"
+	},
+	rconport: 3000,
+	rconpw: "aa",
+	errorchannel: "749943993151782955",
+
+	jailCommand: "/jail ${PLAYERNAME} ${REASON}",
+	banCommand: "/ban ${PLAYERNAME} ${REASON}",
+	unjailCommand: "/unjail ${PLAYERNAME}",
+	unbanCommand: "/unban ${PLAYERNAME}",
+}
+
+export default config
---
+++ src/factorio/join.lua
@@ -0,0 +1,8 @@
+local event = require "event_handler"
+event.on_event(defines.events.on_player_joined_game, function(evt)
+	local player = game.get_player(evt.player_index)
+	game.write_file("ext/awflogging.out", game.table_to_json({
+		type='join',
+		playerName=player.name
+	}))
+end)
