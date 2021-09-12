d7ec217e2e8faf44faad087be86234333518d144
oof2win2 <honza.koco44@gmail.com>
2021-09-12 21:14:02 +0200

    :sparkles: Custom command permission handling

--- src/base/Command.ts
+++ src/base/Command.ts
@@ -1,16 +1,37 @@
 import { SlashCommandBuilder, SlashCommandSubcommandBuilder } from "@discordjs/builders"
 import { CommandInteraction, Message } from "discord.js"
-import { APIMessage } from "discord-api-types"
 import FAGCBot from "./fagcbot"
 
-export interface Command {
+export enum PermissionOverrideType {
+	ROLE = 1,
+	USER = 2,
+}
+type PermissionOverride = {
+	id: string
+	type: PermissionOverrideType.ROLE
+	permission: boolean
+} | {
+	id: string
+	type: PermissionOverrideType.USER
+	permission: boolean
+}
+
+export interface CommandWithSubcommands {
 	data: SlashCommandBuilder,
 	execute: (client: FAGCBot, interaction: CommandInteraction) => Promise<any>
+	permission_overrides?: [PermissionOverride]
+}
+
+export interface Command {
+	data: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">,
+	execute: (client: FAGCBot, interaction: CommandInteraction) => Promise<any>,
+	permission_overrides?: [PermissionOverride]
 }
 
 export interface SubCommand {
 	data: SlashCommandSubcommandBuilder,
 	execute: (client: FAGCBot, interaction: CommandInteraction) => Promise<any>
+	permission_overrides?: [PermissionOverride]
 }
 
 // export type Command <T=void> = {
--- src/commands.ts
+++ src/commands.ts
@@ -1,15 +1,18 @@
 process.chdir("dist")
 
 import { REST } from "@discordjs/rest"
-import { Routes } from "discord-api-types/v9"
+import { Routes, APIApplicationCommand } from "discord-api-types/v9"
 import ENV from "./utils/env.js"
 import fs from "fs/promises"
-import {Command} from "./base/Command.js"
+import { Command } from "./base/Command.js"
 
 const commandCategories = (await fs.readdir("./commands")).filter(command => command.endsWith(".js"))
 const commands = await Promise.all(commandCategories.map(async (commandFile) => {
-	const command: {default: Command} = await import(`./commands/${commandFile}`)
-	return command.default.data.toJSON()
+	const command: { default: Command } = await import(`./commands/${commandFile}`)
+	return {
+		command: command.default.data.toJSON(),
+		permissions: command.default.permission_overrides
+	}
 }))
 
 const rest = new REST({ version: "9" }).setToken(ENV.DISCORD_BOTTOKEN)
@@ -19,15 +22,24 @@ try {
 
 	if (ENV.isDev) {
 		console.log(`Refreshing dev in guild ${ENV.TESTGUILDID}`)
-		await rest.put(
+		const apicommands = await rest.put(
 			Routes.applicationGuildCommands(ENV.CLIENTID, ENV.TESTGUILDID),
-			{ body: commands }
-		)
+			{ body: commands.map(c => c.command) }
+		) as Array<APIApplicationCommand>
+
+		await Promise.all(apicommands.map(async (apicommand) => {
+			const command = commands.find(c => c.command.name === apicommand.name)!
+			if (!command.permissions || !command.permissions.length) return
+			return await rest.put(
+				Routes.applicationCommandPermissions(ENV.CLIENTID, ENV.TESTGUILDID, apicommand.id),
+				{ body: {permissions: command.permissions} }
+			)
+		}))
 	} else {
 		console.log("Refreshing global")
 		await rest.put(
 			Routes.applicationCommands(ENV.CLIENTID),
-			{ body: commands },
+			{ body: commands.map(c => c.command) }
 		)
 	}
 
