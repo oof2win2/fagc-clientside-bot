e1b48e642e32ee7b5633f3884235251bdb9883da
oof2win2 <honza.koco44@gmail.com>
2021-12-08 21:00:43 +0100

    :wastebasket: clean slate

--- src/events/invalidated.ts
+++
@@ -1,5 +0,0 @@
-import FAGCBot from "../base/fagcbot"
-
-export default async (client: FAGCBot): Promise<void> => {
-	client.logger(`${client.user.username} is invalidated: ${new Date().toString().slice(4, 24)}`, "error")
-}
