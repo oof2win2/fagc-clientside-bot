5259c9fe52577e31300ec3a80a30fd3934d01424
oof2win2 <honza.koco44@gmail.com>
2021-12-27 01:42:33 +0100

    ✨ reworked report + revocation handling

--- src/base/wshandler.ts
+++ src/base/wshandler.ts
@@ -2,6 +2,8 @@ import { WebSocketEvents } from "fagc-api-wrapper/dist/WebsocketListener"
 import FAGCBot from "./FAGCBot"
 import { Report } from "fagc-api-types"
 import { MessageEmbed } from "discord.js"
+import { GuildConfig } from "fagc-api-types"
+import { Rcon } from "rcon-client/lib"
 
 interface HandlerOpts<T extends keyof WebSocketEvents> {
 	event: Parameters<WebSocketEvents[T]>[0]
@@ -57,7 +59,11 @@ export const ruleRemoved = async ({ client, event }: HandlerOpts<"ruleRemoved">)
 
 export const report = async ({ client, event }: HandlerOpts<"report">) => {
 	const embed = new MessageEmbed({ ...event.embed, type: undefined })
-	client.infochannels.forEach((guildChannels, guildID) => {
+
+	let shouldPerformActions = false
+	const guildConfigs: GuildConfig[] = []
+	
+	client.infochannels.forEach(async (guildChannels, guildID) => {
 		const guildConfig = client.guildConfigs.get(guildID)
 		if (!guildConfig) return
 		if (
@@ -69,12 +75,43 @@ export const report = async ({ client, event }: HandlerOpts<"report">) => {
 				if (!channel || !channel.isNotDMChannel()) return
 				client.addEmbedToQueue(channel.id, embed)
 			})
-			client.ban(event.report, guildConfig.guildId)
+			shouldPerformActions = true
+			guildConfigs.push(guildConfig)
 		}
+	})
+
+	if (!shouldPerformActions) return // did not match any filters so return
+
+	const isWhitelisted = await client.db.whitelist.findFirst({
+		where: {
+			playername: event.report.playername
+		}
+	})
+	if (isWhitelisted) return // if the player is whitelisted, don't do anything
+
+	// create the record for future safekeeping
+	await client.db.fagcBan.create({
+		data: {
+			id: event.report.id,
+			playername: event.report.playername,
+			brokenRule: event.report.brokenRule,
+			communityID: event.report.communityId,
+		}
+	})
+
+	// ban in guilds that its supposed to
+	guildConfigs.map((guildConfig) => {
+		const reason = client.createBanMessage(event.report, guildConfig.guildId)
+		if (!reason) return // if it is not supposed to do anything in this guild, then it won't do anything
+		client.rcon.rconCommandGuild(`/sc game.ban_player("${event.report.playername}", "${reason}")`, guildConfig.guildId)
 	})
 }
 export const revocation = async ({ client, event }: HandlerOpts<"revocation">) => {
 	const embed = new MessageEmbed({ ...event.embed, type: undefined })
+
+	let shouldPerformActions = false
+	const guildConfigs: GuildConfig[] = []
+
 	await Promise.all(client.infochannels.map(async(guildChannels, guildID) => {
 		const guildConfig = client.guildConfigs.get(guildID)
 		if (!guildConfig) return
@@ -87,71 +124,65 @@ export const revocation = async ({ client, event }: HandlerOpts<"revocation">) =
 				if (!channel || !channel.isNotDMChannel()) return
 				client.addEmbedToQueue(channel.id, embed)
 			})
-			await client.unban(event.revocation, guildID)
-			
-			
-			const privateBans = await client.db.privatebans.findMany({
-				where: {
-					playername: event.revocation.playername,
-				}
-			})
-			if (privateBans.length) {
-				const command = `/ban ${event.revocation.playername} ${privateBans[0].reason}`
-				await client.rcon.rconCommandGuild(command, guildID)
-			} else {
-				const otherBans = await client.fagc.reports.listFiltered({
-					playername: event.revocation.playername,
-					ruleIDs: guildConfig.ruleFilters ?? [],
-					communityIDs: guildConfig.trustedCommunities ?? []
-				})
-				if (otherBans.length) client.ban(otherBans[0], guildID)
-			}
+			shouldPerformActions = true
+			guildConfigs.push(guildConfig)
 		}
 	}))
+
+	if (!shouldPerformActions) return // did not match any filters so return
+
+	const isPrivateBanned = await client.db.privatebans.findFirst({
+		where: {
+			playername: event.revocation.playername
+		}
+	})
+	if (isPrivateBanned) return // if the player is blacklisted, don't do anything
+
+	// remove the report record
+	await client.db.fagcBan.delete({
+		where: {
+			id: event.revocation.id
+		}
+	})
+
+	const otherBan = await client.db.fagcBan.findFirst({
+		where: {
+			playername: event.revocation.playername
+		}
+	})
+
+	// unban the player where it is wanted
+	await Promise.allSettled(
+		guildConfigs.map(async (guildConfig) => {
+			const action = await client.getGuildAction(guildConfig.guildId)
+			if (!action || action.revocation === "none") return // if it is not supposed to do anything in this guild, then it won't do anything
+			// TODO: handling for custom actions
+			client.rcon.rconCommandGuild(`/sc game.unban_player("${otherBan?.playername}")`, guildConfig.guildId)
+		})
+	)
+
+	// if there is another FAGC report that conforms to the rules + communities, perform desired actions with it
+	if (otherBan) {
+		// there are other reports that the player is still banned for
+		const report = await client.fagc.reports.fetchReport({ reportid: otherBan.id })
+		if (!report) return
+		// unban in guilds that its supposed to
+		guildConfigs.map((guildConfig) => {
+			const reason = client.createBanMessage(report, guildConfig.guildId)
+			if (!reason) return // if it is not supposed to do anything in this guild, then it won't do anything
+			// TODO: handling for custom actions
+			client.rcon.rconCommandGuild(`/sc game.ban_player("${report.playername}", "${reason}")`, guildConfig.guildId)
+		})
+		
+	}
+
 }
 
 export const guildConfigChanged = async ({ client, event }: HandlerOpts<"guildConfigChanged">) => {
 	const oldConfig = client.guildConfigs.get(event.guildId)
 
-	if (
-		event.ruleFilters && event.trustedCommunities
-	) {
-		// is an empty array if no old config existed
-		const oldReports = (oldConfig && oldConfig.ruleFilters && oldConfig.trustedCommunities) ? await client.fagc.reports.listFiltered({
-			ruleIDs: oldConfig.ruleFilters,
-			communityIDs: oldConfig.trustedCommunities
-		}) : []
-		const newReports = await client.fagc.reports.listFiltered({
-			ruleIDs: event.ruleFilters,
-			communityIDs: event.trustedCommunities,
-		})
-
-		const oldReportNames = oldReports.map(report => report.playername)
-		const newReportNames = newReports.map(report => report.playername)
-		const unbanPlayers = oldReportNames
-			.filter((playername) => !newReportNames.includes(playername))
-		const newBans = newReportNames
-			.map((playername) => {
-				if (!oldReportNames.includes(playername)) {
-					const report = newReports.find(report => report.playername === playername)!
-					return report
-				}
-				return false
-			})
-			.filter((r): r is Report => Boolean(r))
-		
-		// TODO: custom ban+unban messages
-		const unbanCommand = unbanPlayers
-			.map((playername) => `game.unban_player("${playername}")`)
-			.join(";")
-		const banCommand = newBans
-			.map((report) => `game.ban_player("${report.playername}")`)
-			.join(";")
-		await client.rcon.rconCommandGuild(`/sc ${unbanCommand}`, event.guildId)
-		await client.rcon.rconCommandGuild(`/sc ${banCommand}` , event.guildId)
-	}
+	// TODO: perform an action
 
 	client.guildConfigs.set(event.guildId, event)
 
-	await client.syncCommandPerms(event.guildId)
 }
