8fbbe00acc8e6f14c7829670c1b08d82ac12903e
oof2win2 <honza.koco44@gmail.com>
2022-01-01 01:25:07 +0100

    ✨ potential guild config changed handler

--- src/base/wshandler.ts
+++ src/base/wshandler.ts
@@ -194,17 +194,146 @@ const revocationHandler = async (client: FAGCBot, guildConfigs: GuildConfig[], r
 	}
 }
 
+export const guildConfigChanged = async ({ client, event }: HandlerOpts<"guildConfigChanged">) => {
+	// unban everyone if no filtered rules or communities on the new config
+	if (!event.config.ruleFilters?.length || !event.config.trustedCommunities?.length) {
+		const currentReports = await client.db.fagcBan.findMany()
+		const playernames: Set<string> = new Set()
+		currentReports.map((record) => playernames.add(record.playername))
+		await client.db.fagcBan.deleteMany() // delete all the records
+		// transform playernames into an array and split into chunks of 250
+		const toUnbanChunks = Array.from(playernames).reduce<string[][]>((acc, name) => {
+			const last = acc[acc.length - 1]
+			if (!last || last.length >= 250) {
+				acc.push([])
+			}
+			acc[acc.length - 1].push(name)
+			return acc
+		}, [])
+		// iterate over the chunks with a for loop and unban the players
+		for (const players of toUnbanChunks) {
+		// map over the players and create an unban command for each
+			const command = players
+				.map((playername) => client.createUnbanCommand(playername, event.config.guildId))
+				.join(";")
+			await client.rcon.rconCommandGuild(`/sc ${command}`, event.config.guildId)
+			// sleep for 25ms to prevent servers from getting stuck
+			await new Promise((resolve) => setTimeout(resolve, 25))
+		}
+		return
+	}
+	const newConfig = event.config as typeof event.config & Pick<Required<typeof event.config>, "ruleFilters" | "trustedCommunities">
+
+	// run both at once and then wait for both to finish so no extra time is spent waiting around
+	const [ newReports, currentReports, whitelistRecords, privatebanRecords ] = await Promise.all([
+		client.fagc.reports.listFiltered({
+			ruleIDs: newConfig.ruleFilters,
+			communityIDs: newConfig.ruleFilters
+		}),
+		client.db.fagcBan.findMany(),
+		client.db.whitelist.findMany(),
+		client.db.privatebans.findMany(),
+	])
+
+	const playersToUnban: Set<string> = new Set()
+
+	// array of report IDs that can be removed
+	const toRemoveIDs = currentReports
+		.map((record) => {
+			// check whether the record is no longer in rules that should be forgiven
+			if (!newConfig.ruleFilters.includes(record.brokenRule)) {
+				playersToUnban.add(record.playername)
+				return record.id
+			}
+			if (!newConfig.trustedCommunities.includes(record.communityID)) {
+				playersToUnban.add(record.playername)
+				return record.id
+			}
+			// the record is now deemed appicable and still should still be banned by
+			return false
 		})
-		
+		.filter((id): id is string => Boolean(id))
+	
+	// remove the record of reports that are no longer deemed valid for this community
+	await client.db.fagcBan.deleteMany({
+		where: {
+			id: {
+				in: toRemoveIDs
+			}
+		}
+	})
+
+	const whitelistedPlayers = new Set(whitelistRecords.map((w) => w.playername))
+	const privatebannedPlayers = new Set(privatebanRecords.map((p) => p.playername))
+
+	const toBanReports = newReports
+		.map((report) => {
+			if (whitelistedPlayers.has(report.playername)) return // player is whitelisted, do nothing
+			if (privatebannedPlayers.has(report.playername)) return // player is privately banned, don't store useless data
+
+			playersToUnban.delete(report.playername)
+			return report
+		})
+		.filter((r): r is Report => Boolean(r))
+	
+	// transform playersToUnban into an array and split into chunks of 250
+	const toUnbanChunks = Array.from(playersToUnban).reduce<string[][]>((acc, name) => {
+		const last = acc[acc.length - 1]
+		if (!last || last.length >= 250) {
+			acc.push([])
+		}
+		acc[acc.length - 1].push(name)
+		return acc
+	}, [])
+	// iterate over the chunks with a for loop and unban the players
+	for (const players of toUnbanChunks) {
+		// map over the players and create an unban command for each
+		const command = players
+			.map((playername) => client.createUnbanCommand(playername, event.config.guildId))
+			.join(";")
+		await client.rcon.rconCommandGuild(`/sc ${command}`, event.config.guildId)
+		// sleep for 25ms to prevent servers from getting stuck
+		await new Promise((resolve) => setTimeout(resolve, 25))
 	}
 
-}
+	// transform toBanReports into an array and split into chunks of 250
+	const toBanChunks = Array.from(toBanReports).reduce<Report[][]>((acc, report) => {
+		const last = acc[acc.length - 1]
+		if (!last || last.length >= 250) {
+			acc.push([])
+		}
+		acc[acc.length - 1].push(report)
+		return acc
+	}, [])
+	// iterate over the chunks with a for loop and ban the players
+	for (const reports of toBanChunks) {
+		// map over the players and create a ban command for each
+		const command = reports
+			.map((report) => client.createBanCommand(report, event.config.guildId))
+			.join(";")
+		await client.rcon.rconCommandGuild(`/sc ${command}`, event.config.guildId)
+		// sleep for 25ms to prevent servers from getting stuck
+		await new Promise((resolve) => setTimeout(resolve, 25))
+	}
 
-export const guildConfigChanged = async ({ client, event }: HandlerOpts<"guildConfigChanged">) => {
-	const oldConfig = client.guildConfigs.get(event.config.guildId)
-
-	// TODO: perform an action
+	// create the records of the reports in the db
+	const newRecords = toBanReports
+		.map((report) => `('${report.id}', '${report.playername}', '${report.brokenRule}', '${report.communityId}')`)
+	// split newRecords into arrays of 250 and join them with a comma
+	const newRecordChunks = Array.from(newRecords).reduce<string[][]>((acc, record) => {
+		const last = acc[acc.length - 1]
+		if (!last || last.length >= 250) {
+			acc.push([])
+		}
+		acc[acc.length - 1].push(record)
+		return acc
+	}, [])
+	// iterate over the chunks with a for loop and insert the records into the db
+	for (const records of newRecordChunks) {
+		await client.db.$executeRawUnsafe(`INSERT INTO \`main\`.\`fagcban\` VALUES \t${records.join(",\n\t")};`)
+		// wait for 50ms to allow other queries to run
+		await new Promise((resolve) => setTimeout(resolve, 50))
+	}
 
 	client.guildConfigs.set(event.config.guildId, event.config)
-
 }
